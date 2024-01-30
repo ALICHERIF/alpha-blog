a1022f61aa155828150c0281a7323ebb84c386a9
ALICHERIF <adem.mohamed@hotmail.fr>
2024-01-30 17:12:24 +0100

    cleanup layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 @user = User.find(params[:id])
       if @user.update(user_params)
         flash[:notice] = "User has been updated successfully."
-        redirect_to articles_path
+        redirect_to @user
       else
         render :edit, status: :unprocessable_entity
       end
