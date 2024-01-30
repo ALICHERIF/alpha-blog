72dd0cd0103aa849777560e7483a5c1ced7a85b8
ALICHERIF <adem.mohamed@hotmail.fr>
2024-01-30 11:24:42 +0100

    add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,20 @@ class UsersController < ApplicationController
     def new
       @user = User.new
     end
+
+    def edit
+@user = User.find(params[:id])
+    end
+    def update
+@user = User.find(params[:id])
+      if @user.update(user_params)
+
+        flash[:notice] = "User has been updated successfully."
+        redirect_to articles_path
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
     def create
       @user = User.new(user_params)
           if @user.save
