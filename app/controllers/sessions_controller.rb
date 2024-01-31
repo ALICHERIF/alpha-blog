61f7ee71744e07620062f43c6f9fb2029bbd2ecc
ALICHERIF <adem.mohamed@hotmail.fr>
2024-01-31 13:36:39 +0100

    create and destroy sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,22 @@ class SessionsController < ApplicationController
 def new
 end
 def create
-  binding.break
+user =User.find_by(email: params[:session][:email].downcase)
+  if user && user.authenticate(params[:session][:password])
+    session[:user_id] = user.id
+flash[:notice] = "successful login "
+redirect_to user
+  else
+    flash.now[:alert] = "there was something wrong with your login details "
+    render 'new'
+    #binding.break
+  end
 end
 def destroy
+  session[:user_id] = nil
+  #binding.break
+flash[:notice] = "successful logout "
+redirect_to root_path
 end
 
 end
