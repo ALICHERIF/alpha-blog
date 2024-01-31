aa26bc600b3a64f69528aa9a11c5d33465d45eb1
ALICHERIF <adem.mohamed@hotmail.fr>
2024-01-31 17:50:39 +0100

    authentication helper method ;)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 def new
 end
 def create
-user =User.find_by(email: params[:session][:email].downcase)
+user = User.find_by(email: params[:session][:email].downcase)
   if user && user.authenticate(params[:session][:password])
     session[:user_id] = user.id
 flash[:notice] = "successful login "
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+before_action :set_user, only: [:show, :edit, :update]
+
     def show
-@user = User.find(params[:id])
+
 @articles = @user.articles.paginate(page: params[:page], per_page:3)
     end
     def index
@@ -11,10 +13,10 @@ class UsersController < ApplicationController
     end
 
     def edit
-@user = User.find(params[:id])
+
     end
     def update
-@user = User.find(params[:id])
+
       if @user.update(user_params)
         flash[:notice] = "User has been updated successfully."
         redirect_to @user
@@ -25,6 +27,7 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
           if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "#{@user.username} has created successfully."
             #binding.break
             redirect_to articles_path
@@ -38,5 +41,7 @@ private
       def user_params
         params.require(:user).permit(:username, :email, :password)
       end
-
+def set_user
+@user = User.find(params[:id])
+end
 end
